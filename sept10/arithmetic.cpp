930053260c0388251bb4241d655b97296c28808b
Charlotte Curtis <ccurtis@mtroyal.ca>
2025-09-10 09:50:33 -0600

    Updating hello and adding arithmetic examples

---
+++ sept10/arithmetic.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // this is a single line comment
+    int y = 5 / 2;
+    cout << "y = " << y << endl;
+    double x = 5 / 2;
+    cout << "x = "
+         << x
+         << endl;
+    
+    x = 5 / 2.0;
+    cout << "x = " << x << endl;
+    
+    return 0;
+}
--- sept10/hello.cpp
+++ sept10/hello.cpp
@@ -3,5 +3,10 @@ using namespace std;
 
 int main() {
     cout << "Hello, world!\n";
+    cout << "a";
+    char a = 97;
+    cout << a << '\n';
+    int a_i = 'a';
+    cout << a_i << '\n';
     return 0;
 }
